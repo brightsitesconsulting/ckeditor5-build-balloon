d07618aebd7fb186ebc587176ace58cb01ea3750
Tom Marulak <sitorush@gmail.com>
2020-04-06 16:19:45 +0100

    Ability to set ckeditor special character from outside of the editor

--- src/plugin/SpecialCharacters/SpecialCharacters.js
+++ src/plugin/SpecialCharacters/SpecialCharacters.js
@@ -1,6 +1,17 @@
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview';
+import Command from '@ckeditor/ckeditor5-core/src/command';
 import omegaIcon from './omega.svg';
+
+class AttributeCommand extends Command {
+	constructor( editor, callback ) {
+		super( editor );
+		this.onPluginExecute = callback;
+	}
+	execute() {
+		this.onPluginExecute();
+	}
+}
 
 export default class SpecialCharaters extends Plugin {
 	constructor( editor ) {
@@ -13,6 +24,26 @@ export default class SpecialCharaters extends Plugin {
 
 		const callbacks = editor.config.get( 'specialCharacters.callbacks' );
 
+		const onExecutePlugin = ( cbs, currentEditor ) => () => {
+			if ( !callbacks.searchSpecialCharactersRequested ) {
+				throw new Error( 'No callback searchSpecialCharactersRequested provided' );
+			}
+			callbacks.searchSpecialCharactersRequested().then( data => {
+				if ( !data ) {
+					return;
+				}
+
+				const model = currentEditor.model;
+				const selection = model.document.selection;
+				model.change( writer => {
+					writer.insertText( data, selection.getLastPosition(), 'after' );
+				} );
+			} ).catch( ex => {
+				console.error( ex );
+			} );
+		};
+
+		editor.commands.add( 'specialCharacters', new AttributeCommand( editor, onExecutePlugin( callbacks, editor ) ) );
 		editor.ui.componentFactory.add( 'specialCharacters', locale => {
 			const view = new ButtonView( locale );
 
@@ -23,24 +54,7 @@ export default class SpecialCharaters extends Plugin {
 			} );
 
 			// Callback executed once the image is clicked.
-			view.on( 'execute', () => {
-				if ( !callbacks.searchSpecialCharactersRequested ) {
-					throw new Error( 'No callback searchSpecialCharactersRequested provided' );
-				}
-				callbacks.searchSpecialCharactersRequested().then( data => {
-					if ( !data ) {
-						return;
-					}
-
-					const model = editor.model;
-					const selection = model.document.selection;
-					model.change( writer => {
-						writer.insertText( data, selection.getLastPosition(), 'after' );
-					} );
-				} ).catch( ex => {
-					console.error( ex );
-				} );
-			} );
+			view.on( 'execute', onExecutePlugin( callbacks, editor ) );
 
 			return view;
 		} );
